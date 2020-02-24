ad718856958677c72e0eee8ca65ba4ea042544cb
Plattsy85 <jamieplatts@jamies-mbp.lan>
2020-02-24 11:37:03 +0000

    Form test

--- beyond-static/pages/index.vue
+++ beyond-static/pages/index.vue
@@ -26,6 +26,21 @@
 
         <h1>Contact us</h1>
 
+        <form name="contacttest" netlify>
+          <p>
+            <label>Name <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Email <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Message <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+
       </div>
     </div>
   </div>
